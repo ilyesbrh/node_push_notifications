00b225c9868c7547016e975cc197e2371669359b
desktop <brhilyes@gmail.com>
2020-04-30 01:54:56 +0100

    adaptation v3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 // Set static path
 app.use(express.static(path.join(__dirname, "client")));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
 
   res.sendFile(path.join(__dirname , 'client'));
 });
