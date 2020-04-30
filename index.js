60b86065067f583064dcb471238c35cd188e7d4d
desktop <brhilyes@gmail.com>
2020-04-30 01:52:41 +0100

    adaptation v2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 // Set static path
 app.use(express.static(path.join(__dirname, "client")));
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
 
   res.sendFile(path.join(__dirname , 'client'));
 });
@@ -42,6 +42,6 @@ app.post("/subscribe", (req, res) => {
     .catch(err => console.error(err));
 });
 
-const port = 5000;
+const port = process.env.port;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
